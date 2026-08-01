83439c9883b7a35cc4051789056a543c7acf7593
Sword-Galaxy-Co <swordgalaxynovelcode@gmail.com>
2026-07-31 21:39:47 -0500

    Initial commit

---
+++ main menu button.gd
@@ -0,0 +1,16 @@
+extends Control
+
+func _ready() -> void:
+	pass
+
+
+
+func _on_new_game_pressed() -> void:
+	var t = FileAccess
+
+func _on_load_game_pressed() -> void:
+	#if FileAccess
+	pass # Replace with function body.
+
+func _on_quit_button_pressed() -> void:
+	pass # Replace with function body.
