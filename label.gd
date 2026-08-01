ef16fe20b06a5406bab6addd6d5a07cd5de25611
Sword-Galaxy-Co <swordgalaxynovelcode@gmail.com>
2026-07-31 23:41:56 -0500

    day 1

--- label.gd
+++ label.gd
@@ -6,20 +6,20 @@ extends Label
 @onready var QB: Button = %"quit button"
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	get_tree().call_group("Core","hide")
-	L.visible_characters = 0
-	NG.disabled = true
-	LG.disabled = true
-	QB.disabled = true
-	var current = 0
-	await get_tree().create_timer(1).timeout
-	while current <= L.text.length():
-		L.visible_characters = current
-		current += 1
-		await get_tree().create_timer(.2).timeout
-	print("Title loaded")
-	await get_tree().create_timer(.1).timeout
-	NG.disabled = false
-	LG.disabled = false
-	QB.disabled = false
+#func _ready() -> void:
+	#get_tree().call_group("Core","hide")
+	#L.visible_characters = 0
+	#NG.disabled = true
+	#LG.disabled = true
+	#QB.disabled = true
+	#var current = 0
+	#await get_tree().create_timer(1).timeout
+	#while current <= L.text.length():
+		#L.visible_characters = current
+		#current += 1
+		#await get_tree().create_timer(.2).timeout
+	#print("Title loaded")
+	#await get_tree().create_timer(.1).timeout
+	#NG.disabled = false
+	#LG.disabled = false
+	#QB.disabled = false
--- main menu button.gd
+++ main menu button.gd
@@ -1,16 +1,38 @@
 extends Control
 
-func _ready() -> void:
-	pass
-
-
+var save = {
+}
 
 func _on_new_game_pressed() -> void:
-	var t = FileAccess
+	save = FileAccess.READ
+	var file = FileAccess.open("user://Kon.json",FileAccess.WRITE)
+	file.store_string(JSON.stringify(save,"\t"))
 
 func _on_load_game_pressed() -> void:
-	#if FileAccess
+	$"Load Game".text = "Work in Progress"
+	$"Load Game".disabled = true
 	pass # Replace with function body.
 
+var click =0
+
 func _on_quit_button_pressed() -> void:
+	match click:
+		0:
+			$"quit button".text = "why"
+			click +=1
+		1,2,3,4,5:
+			$"quit button".text = ""
+			click +=1 
+		6:
+			$"quit button".text = "Do you want to leave that badly"
+			click +=1
+		7:
+			$"quit button".text = "Ah I see, well then good bye"
+			click +=1
+		8:
+			FileAccess.open("user://test.ending",FileAccess.WRITE).store_string("Didn't want to stay")
+			get_tree().quit()
+			
+		_:
+			print("error")
 	pass # Replace with function body.
