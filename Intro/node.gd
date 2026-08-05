2a257930a3c074428e863d5192e07224c44bdfd4
Sword-Galaxy-Co <swordgalaxynovelcode@gmail.com>
2026-08-04 23:01:12 -0500

    day 5

--- Intro/node.gd
+++ Intro/node.gd
@@ -2,6 +2,12 @@ extends Node
 #var quick_debug =true
 @onready var panel: Panel = $Panel/Panel
 var story_code_int:int = 1
+@onready var bootup: AudioStreamPlayer2D = $Panel/Node/Panel/bootup
+@onready var animation_player: AnimationPlayer = $Panel/Node/Panel/AnimationPlayer
+
+func _ready() -> void:
+	animation_player.play("anime 1");
+	bootup.play(0.0)
 func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.is_pressed():
 		if event.button_index == MOUSE_BUTTON_LEFT:
