99ad2abcffbaf85683fc1d89303c26a08b15027b
Sword-Galaxy-Co <swordgalaxynovelcode@gmail.com>
2026-08-10 01:14:48 -0500

    day 8
    these are days that i have worked on the Project

--- Intro/node.gd
+++ Intro/node.gd
@@ -5,7 +5,6 @@ var story_code_int = Variable.story_index_num
 @onready var main_text: RichTextLabel = $Panel/Node/Panel/Panel/RichTextLabel
 @onready var button_1_text: RichTextLabel = $"Panel/button 1/button 1 text"
 
-
 func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.is_pressed():
 		if event.button_index == MOUSE_BUTTON_LEFT:
@@ -13,21 +12,21 @@ func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) 
 			#if
 			story_code_int += 1
 
-		if event.button_index == MOUSE_BUTTON_RIGHT and EngineDebugger.is_active() == true:
+		elif event.button_index == MOUSE_BUTTON_RIGHT and EngineDebugger.is_active() == true:
 			print("-");
 			if story_code_int >= 0 and story_code_int != 0:
 				story_code_int -= 1
 				print("fail?")
 			elif story_code_int == 0:
 				get_tree().quit()
-		if event.button_index == MOUSE_BUTTON_LEFT or MOUSE_BUTTON_RIGHT:
-			update_button_and_text()
+		update_button_and_text()
 
 func update_button_and_text():
 	print(story_code_int)
 	if story_code_int == Variable.S["Intro"].size() or story_code_int <= Variable.S["Intro"].size():
-		#print(Variable.S["Intro"][story_code_int])
+		print(Variable.S["Intro"][story_code_int])
 		print("???")
+		Variable.S["Intro"]
 		pass
 	elif story_code_int >= Variable.S["Intro"].size():
 		print('test')
@@ -37,4 +36,6 @@ func update_button_and_text():
 
 func _ready() -> void:
 	update_button_and_text()
+	button_1_text.text =""
+	main_text.text =""
 	pass
